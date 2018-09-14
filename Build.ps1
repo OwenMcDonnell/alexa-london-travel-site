5ccbe282335adc2f831772af9b584b49724b489c
Owen McDonnell <7119543+OwenMcDonnell@users.noreply.github.com>
2018-09-13 22:54:54 -0700

    Update Build.ps1

--- Build.ps1
+++ Build.ps1
@@ -89,7 +89,7 @@ function DotNetTest {
             -oldstyle `
             -register:user `
             -skipautoprops `
-            `"-filter:+[LondonTravel.Site]* -[LondonTravel.Site.Tests]*`"
+            -filter:"+[LondonTravel.Site]* -[LondonTravel.Site.Tests]*"
 
         if ($LASTEXITCODE -eq 0) {
             & $reportGeneratorPath `
